34e7fc34740433467faf6843e18e1260fffe2859
유수영 <cria200@naver.com>
2022-04-01 12:44:10 +0900

    mainPage 레이아웃 1차 완료

--- src/Constant/messages.ts
+++ src/Constant/messages.ts
@@ -1,5 +1,5 @@
 export default {
-    whatIsMyDrinkIt: '내 안의 드링킷은?',
+    whatIsMyDrinkIt: '내 안의 드링킷🥂은?',
     startTest: 'DITI 테스트 시작하기',
     enquiryInformation: `위 테스트는 정기 뉴스레터 '드링킷'의 내용을 기반으로 제작되었습니다.`,
     testEnquiry: '테스트 관련 문의는 cria2000@naver.com으로,',
