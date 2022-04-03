02901731ce856b5ac494d2e7439d20eebe48f27c
유수영 <cria200@naver.com>
2022-04-04 03:18:42 +0900

    router basename추가

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="https://cria2000.github.io/drink-it-test/">
         <React.StrictMode>
             <App />
         </React.StrictMode>
